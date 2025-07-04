95a7d436e53d782a7dfa388041e9257f5a88f8d1
Максим Ничай <nychaimaxim@gmail.com>
2025-07-04 21:24:21 +0300

    New function addTask

--- Examen C++/Examen C++.cpp
+++ Examen C++/Examen C++.cpp
@@ -43,4 +43,48 @@ void initCategories() {
     }
 }
 
-//ава
+void addTask() {
+    if (taskCount >= MAX_TASKS) {
+        cout << "Більше завдань додати не можна.\n";
+        return;
+    }
+
+    Task t;
+    cout << "Назва: ";
+    getline(cin, t.title);
+    cout << "Опис: ";
+    getline(cin, t.description);
+    cout << "Дедлайн (рррр-мм-дд): ";
+    getline(cin, t.deadline);
+
+    int pr;
+    cout << "Пріоритет (1-LOW,2-MEDIUM,3-HIGH): ";
+    cin >> pr;
+    cin.ignore();
+    t.priority = (Priority)pr;
+
+    int tp;
+    cout << "Тип (1-WORK,2-PERSONAL,3-STUDY,4-OTHER): ";
+    cin >> tp;
+    cin.ignore();
+    t.type = (TaskType)tp;
+
+    cout << "ID користувача: ";
+    cin >> t.user.id;
+    cin.ignore();
+    cout << "Ім'я: ";
+    getline(cin, t.user.name);
+    cout << "Прізвище: ";
+    getline(cin, t.user.forname);
+    cout << "Email: ";
+    getline(cin, t.user.email);
+
+    t.done = false;
+
+    allTasks[taskCount++] = t;
+
+    int idx = t.type - 1;
+    categories[idx].tasks[categories[idx].count++] = t;
+
+    cout << "Завдання додано.\n";
+}
